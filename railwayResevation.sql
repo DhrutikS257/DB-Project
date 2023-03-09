3e50ecda3425338ea18bd53a48a8bce00c836939
Dhrutik-S <solankidhrutik0602@gmail.com>
2023-03-08 19:59:36 -0600

    Added query for Q5, but need to remove passenger name and change it with num of passenger

--- railwayResevation.sql
+++ railwayResevation.sql
@@ -1,8 +1,16 @@
+-- Q2
 SELECT t.train_name
 FROM Passenger as p, Booked as b, Train as t
 WHERE p.first_name = 'Art' AND p.last_name = 'Venere' AND p.SSN = b.SSN 
         AND b.status = 'Booked' AND t.train_number = b.train_number;
 
+-- Q3
 SELECT p.first_name, p.last_name
 FROM Train_Status as ts, Booked as b, Passenger as p
 WHERE ts.train_date = '2022/19/02' AND b.status = 'Booked' AND b.SSN = p.SSN;
+
+-- Q5
+SELECT p.first_name, p.last_name, t.train_name, ts.train_date, t.train_number
+FROM Booked as b, Train as t, Train_Status as ts, Passenger as p
+WHERE b.status = 'Booked' AND b.train_number = t.train_number 
+        AND ts.train_name = t.train_name AND p.SSN = b.SSN;
