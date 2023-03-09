1bb846330c92a349fe8ceb28ec647a5e128a1d4f
Dhrutik-S <solankidhrutik0602@gmail.com>
2023-03-09 17:21:12 -0600

    Somewhat fixed Q4, still need to work on it

--- railwayResevation.sql
+++ railwayResevation.sql
@@ -11,9 +11,8 @@ WHERE ts.train_date = '2022/19/02' AND b.status = 'Booked' AND b.SSN = p.SSN;
 
 -- Q4
 SELECT p.first_name, p.last_name
-FROM Train as t, Passenger as p
-WHERE p.bdate 
-BETWEEN '1963/03/09' AND '1973/03/09';
+FROM Passenger as p, Train as t
+WHERE p.bdate BETWEEN '1963/03/09' AND '1973/03/09' AND t.train_name = 'Orient Express';
 
 -- Q5
 SELECT COUNT(p.first_name), t.train_name, ts.train_date, t.train_number
@@ -43,5 +42,3 @@ SELECT p.first_name, p.last_name
 FROM Train as t, Passenger as p, Booked as b
 WHERE t.available_on LIKE '%Thursday%' AND b.status = 'Booked' AND p.SSN = b.SSN
 ORDER BY p.first_name ASC;
-
-
